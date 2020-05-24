70f16ca4a5b1addae1ae426defe264bad3b88b82
lzricardo <lzricardo.ecomp@gmail.com>
2020-05-23 22:43:39 -0300

    :white_check_mark::art: Improve tests

--- __tests__/unit/candidature.test.js
+++ __tests__/unit/candidature.test.js
@@ -5,6 +5,10 @@ beforeEach(async () => {
     await truncate();
 });
 
+afterAll(async () => {
+    await truncate();
+});
+
 describe('Candidature\'s model\'s operations', () => {
     it('should be persist a candidature',  async (done) => {
         const candidatureSaved = await Candidature.create({
@@ -21,65 +25,77 @@ describe('Candidature\'s model\'s operations', () => {
         done();
     });
 
-    it('should be send message error for save candidature without any required fields', async (done) => {
-        await Candidature.create({
-            position_name: 'Senior Software Engineer',
-            situation: 'hired',
-        }).catch(e => {
+    it('should be send message error for save candidature without any required fields', async () => {
+        try {
+            await Candidature.create({
+            })
+        } catch (e) {
             expect(e.message).toMatch('error');
-        });
+        }
 
-        await Candidature.create({
-            applicant_email: 'lzricardo.ecomp@gmail.com',
-            situation: 'hired',
-        }).catch(e => {
+        try {
+            await Candidature.create({
+                applicant_email: 'lzricardo.ecomp@gmail.com',
+                situation: 'hired',
+            });
+        } catch (e) {
             expect(e.message).toMatch('error');
-        });
+        }
 
-        await Candidature.create({
-            applicant_email: 'lzricardo.ecomp@gmail.com',
-            position_name: 'Senior Software Engineer',
-        }).catch(e => {
-            expect(e.message).toMatch('error');
-        });
-        done();
+        let mockDontExecute = jest.fn();
+
+        //This don't throw error because exist default situation.
+        try {
+            await Candidature.create({
+                applicant_email: 'lzricardo.ecomp@gmail.com',
+                position_name: 'Senior Software Engineer',
+            });
+        } catch (e) {
+            mockDontExecute();
+        }
+
+        expect(mockDontExecute.mock.calls.length).toBe(0);
     });
 
-    it('should be send message error for save candidature with invalid value for any fields', async (done) => {
-        await Candidature.create({
-            applicant_email: 'lzricardo.ecomp@gmail.com',
-            position_name: 'Senior Software Engineer',
-            situation: 1,
-        }).catch(e => {
+    it('should be send message error for save candidature with invalid value for any fields', async () => {
+        try {
+            await Candidature.create({
+                applicant_email: 'lzricardo.ecomp@gmail.com',
+                position_name: 'Senior Software Engineer',
+                situation: 1,
+            });
+        } catch (e) {
             expect(e.message).toMatch('error');
-            done();
-        });
+        }
 
-        await Candidature.create({
-            applicant_email: 'lzricardo.ecomp@gmail.com',
-            position_name: '',
-            situation: 'hired',
-        }).catch(e => {
+        try {
+            await Candidature.create({
+                applicant_email: 'lzricardo.ecomp@gmail.com',
+                position_name: '',
+                situation: 'hired',
+            });
+        } catch (e) {
             expect(e.message).toMatch('error');
-            done();
-        });
+        }
 
-        await Candidature.create({
-            applicant_email: 'lzricardo.ecompgmail.com',
-            position_name: 'Senior Software Engineer',
-            situation: 'hired',
-        }).catch(e => {
+        try {
+            await Candidature.create({
+                applicant_email: 'lzricardo.ecompgmail.com',
+                position_name: 'Senior Software Engineer',
+                situation: 'hired',
+            });
+        } catch (e) {
             expect(e.message).toMatch('error');
-            done();
-        });
+        }
 
-        await Candidature.create({
-            applicant_email: '',
-            position_name: 'Senior Software Engineer',
-            situation: 'hired',
-        }).catch(e => {
+        try {
+            await Candidature.create({
+                applicant_email: '',
+                position_name: 'Senior Software Engineer',
+                situation: 'hired',
+            });
+        } catch (e) {
             expect(e.message).toMatch('error');
-            done();
-        });
+        }
     });
 });
